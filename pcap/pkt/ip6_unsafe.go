5b42b3ecac22583e17cc8ad71b223f8ea7befd31
Preetam Jinka <pj@isomero.us>
2013-09-20 13:25:15 -0400

    Tweak build flags

--- pcap/pkt/ip6_unsafe.go
+++ pcap/pkt/ip6_unsafe.go
@@ -1,8 +1,8 @@
-// Copyright 2013 The golibpcap Authors. All rights reserved.                      
-// Use of this source code is governed by a BSD-style                              
-// license that can be found in the LICENSE file.                                  
+// Copyright 2013 The golibpcap Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
-// +build !safe,!appengine
+// +build linux,!safe,!appengine
 
 package pkt
 
--- pcap/pkt/testHelp.go
+++ pcap/pkt/testHelp.go
@@ -1,8 +1,8 @@
-// Copyright 2012 The golibpcap Authors. All rights reserved.                      
-// Use of this source code is governed by a BSD-style                              
+// Copyright 2012 The golibpcap Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !safe,!appengine
+// +build linux,!safe,!appengine
 
 package pkt
 
